c4c2d0726c2201fd78d1390ec9dbd7f9df4f2a36
Elnur21 <elnurme@code.edu.az>
2023-07-12 11:32:00 +0400

    home slideshowe without next-prev button shapes

---
+++ assets/js/swiper.js
@@ -0,0 +1,23 @@
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  slidesPerView: 1,
+  grabCursor: true,
+  autoplay: {
+    delay: 1500,
+    disableOnInteraction: false,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 30,
+    },
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+});
