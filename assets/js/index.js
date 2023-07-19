8b7cd9a68a1380212363b1de922bee954c08db12
Elnur21 <elnurme@code.edu.az>
2023-07-19 18:59:46 +0400

    loading

--- assets/js/index.js
+++ assets/js/index.js
@@ -28,18 +28,39 @@ function updateScrollProgress() {
   }%,white ${scrolled * 100}%)`;
 }
 
-
 let hamburger = document.querySelector(".btns-header .icon-btn-light");
 let sidebar = document.querySelector(".sidebar");
 let left_sidebar = document.querySelector(".left-sidebar");
-let close_btn = document.querySelector(".close")
-hamburger.addEventListener("click",()=>{
+let close_btn = document.querySelector(".close");
+hamburger.addEventListener("click", () => {
   sidebar.style.transform = "translateX(0%)";
-  left_sidebar.style.display="block"
-})
-left_sidebar.addEventListener("click",close)
-close_btn.addEventListener("click",close)
+  left_sidebar.style.display = "block";
+});
+left_sidebar.addEventListener("click", close);
+close_btn.addEventListener("click", close);
 function close() {
   sidebar.style.transform = "translateX(100%)";
-  left_sidebar.style.display="none";
+  left_sidebar.style.display = "none";
 }
+
+let currentCursorPos;
+let cursorEl = document.querySelector("#cursor");
+
+window.addEventListener("mousemove", (event) => {
+  currentCursorPos = { x: event.clientX, y: event.clientY };
+  cursorEl.style.transform = `translate(${currentCursorPos.x}px, ${currentCursorPos.y}px)`;
+});
+
+let right = document.querySelector("#right");
+let left = document.querySelector("#left");
+
+document.addEventListener("DOMContentLoaded", () => {
+  left.style.transform = "translateX(-100%)";
+  right.style.transform = "translateX(100%)";
+  setTimeout(()=>{
+    document.querySelector(".circle").style.display="none"
+  },500)
+  setTimeout(()=>{
+    document.querySelector(".loading").style.display="none"
+  },1250)
+});
